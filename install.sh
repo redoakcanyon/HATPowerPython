4e8d5391b8a3b9d62297fd963e1d6684494af9a3
Terry Garyet <terry@redoakcanyon.com>
2015-11-15 10:35:09 -0600

    Update install.sh
    fixed typos in update-rc.d commands

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ sudo cp killPower* /etc/init.d
 
 #Install the scripts into the startup/shutdown sequence. update-rc.d will issue some warnings, but don't worry about them.
 
-sudo update-rc.d killPower default
-sudo update-rc.d monitorPS default
-sudo update-rc.d monitorPB default
+sudo update-rc.d killPower defaults
+sudo update-rc.d monitorPS defaults
+sudo update-rc.d monitorPB defaults
 
 
 # The python scripts need the wiringpi2 class.  Installe it as in (from http://raspi.tv/how-to-install-wiringpi2-for-python-on-the-raspberry-pi):
